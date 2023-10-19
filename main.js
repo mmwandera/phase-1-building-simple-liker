59720bd81ab3660b457f8e71544dc53aa0d67bca
mmwandera <mmwandera74@gmail.com>
2023-10-19 08:14:50 +0300

    Completed assignment

--- main.js
+++ main.js
@@ -3,9 +3,36 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener('DOMContentLoaded', () => {
+  const errorModal = document.getElementById('modal');
+  errorModal.classList.add('hidden');
 
+  let isLiked = false
 
-
+  const heartElement = document.querySelector('.like-glyph');
+  heartElement.addEventListener('click', () => {
+    mimicServerCall()
+      .then(() => {
+        if (isLiked) {
+          heartElement.innerHTML = EMPTY_HEART;
+          heartElement.classList.remove('activated-heart');
+          isLiked = false;
+        } else {
+          heartElement.innerHTML = FULL_HEART;
+          heartElement.classList.add('activated-heart');
+          isLiked = true;
+        }
+      })
+      .catch((error) => {
+        const modalMessage = document.querySelector('#modal-message');
+        modalMessage.textContent = error;
+        errorModal.classList.remove('hidden');
+        setTimeout(() => {
+          errorModal.classList.add('hidden');
+        }, 3000);
+      });
+  });
+});
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
